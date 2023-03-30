50de9d89637cbf72c7323c5af5f23c5a38fb4d88
Farros <86004023+farroshsy@users.noreply.github.com>
2023-03-30 14:05:53 +0700

    feat: reviewed thread and multiprocessing server

--- server_multiprocessing_http.py
+++ server_multiprocessing_http.py
@@ -1,11 +1,10 @@
-import os
 from socket import *
 import socket
-import threading
 import time
 import sys
 import logging
-
+import multiprocessing
+import threading
 from http import HttpServer
 
 httpserver = HttpServer()
@@ -46,22 +45,25 @@ class ProcessTheClient(threading.Thread):
 		self.connection.close()
 
 
-class Server(threading.Thread):
+
+class Server(multiprocessing.Process):
 	def __init__(self):
 		self.the_clients = []
 		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		threading.Thread.__init__(self)
+		multiprocessing.Process.__init__(self)
 
 	def run(self):
-		self.my_socket.bind(('0.0.0.0', 8898))
+		self.my_socket.bind(('0.0.0.0', 8892))
 		self.my_socket.listen(1)
 		while True:
 			self.connection, self.client_address = self.my_socket.accept()
 			logging.warning("connection from {}".format(self.client_address))
+
 			clt = ProcessTheClient(self.connection, self.client_address)
 			clt.start()
 			self.the_clients.append(clt)
+
 
 
 def main():
--- server_multiprocessing_http_secure.py
+++ server_multiprocessing_http_secure.py
@@ -1,13 +1,14 @@
-import os
 import socket
-import threading
-import time
+import os
 import logging
+import multiprocessing
+import threading
 import ssl
 from http import HttpServer
-from multiprocessing import Process
+
 
 httpserver = HttpServer()
+
 
 class ProcessTheClient(threading.Thread):
     def __init__(self, connection, address):
@@ -16,26 +17,26 @@ class ProcessTheClient(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        rcv=""
+        rcv = ""
         while True:
             try:
                 data = self.connection.recv(32)
                 if data:
-                    #merubah input dari socket (berupa bytes) ke dalam string
-                    #agar bisa mendeteksi \r\n
+                    # merubah input dari socket (berupa bytes) ke dalam string
+                    # agar bisa mendeteksi \r\n
                     d = data.decode()
-                    rcv=rcv+d
-                    if rcv[-2:]=='\r\n':
-                        #end of command, proses string
+                    rcv = rcv + d
+                    if rcv[-2:] == '\r\n':
+                        # end of command, proses string
                         logging.warning("data dari client: {}" . format(rcv))
                         hasil = httpserver.proses(rcv)
-                        #hasil akan berupa bytes
-                        #untuk bisa ditambahi dengan string, maka string harus di encode
-                        hasil=hasil+"\r\n\r\n".encode()
+                        # hasil akan berupa bytes
+                        # untuk bisa ditambahi dengan string, maka string harus di encode
+                        hasil = hasil + "\r\n\r\n".encode()
                         logging.warning("balas ke  client: {}" . format(hasil))
-                        #hasil sudah dalam bentuk bytes
+                        # hasil sudah dalam bentuk bytes
                         self.connection.sendall(hasil)
-                        rcv=""
+                        rcv = ""
                         self.connection.close()
                 else:
                     break
@@ -43,19 +44,35 @@ class ProcessTheClient(threading.Thread):
                 pass
         self.connection.close()
 
-def run_server():
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.bind(('0.0.0.0', 8899))
-    my_socket.listen(1)
-    while True:
-        connection, client_address = my_socket.accept()
-        try:
-            clt = ProcessTheClient(connection, client_address)
+
+class Server(multiprocessing.Process):
+    def __init__(self, hostname='testing.net'):
+        self.the_clients = []
+        self.hostname = hostname
+        cert_location = os.getcwd() + '/certs/'
+        self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        self.context.load_cert_chain(certfile=cert_location + 'domain.crt',
+                                     keyfile=cert_location + 'domain.key')
+        multiprocessing.Process.__init__(self)
+
+    def run(self):
+        self.my_socket = self.context.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_side=True)
+        self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.my_socket.bind(('0.0.0.0', 8891))
+        self.my_socket.listen(1)
+        while True:
+            self.connection, self.client_address = self.my_socket.accept()
+            logging.warning("connection from {}".format(self.client_address))
+
+            clt = ProcessTheClient(self.connection, self.client_address)
             clt.start()
-        except:
-            logging.exception("Error: ")
-    connection.close()
+            self.the_clients.append(clt)
+
+
+def main():
+    svr = Server()
+    svr.start()
+
 
 if __name__ == "__main__":
-    run_server()
+    main()
